c9503147a56a80a821b33ec48f934fa22a58c3d3
mikeharten <mike.harten@gmail.com>
2015-09-04 22:25:11 -0600

    register front end parts.

---
+++ js/services.js
@@ -0,0 +1,5 @@
+var app = angular.module('SocialScriptureApp',[]);
+
+app.factory('Register', function() {
+
+});
--- js/ss.js
+++ js/ss.js
@@ -1,8 +1,37 @@
-var myApp = angular.module('ScriptureApp',[]);
+var myApp = angular.module('SocialScriptureApp',[]);
 
-myApp.controller('ScriptureCtrl', ['$scope', function($scope) {
-  $scope.book = "1-ne";
-  $scope.chapter = "10";
-  $scope.verse = "15";
-  $scope.source = "https://www.lds.org/scriptures/bofm/1-ne/2.1?lang=eng";
+myApp.controller('RegisterCtrl', ['$scope', '$http', function(scope, $http){
+	scope.email = "junk@junk.com";
+	scope.firstName = "First Name";
+	scope.lastName = "Last Name";
+	scope.username = "Username";
+	scope.password = "Password";
+
+	scope.postResponse = function () {
+		var postData = {
+			email : scope.email,
+			firstName : scope.firstName,
+			lastName : scope.lastName,
+			userName : scope.username,
+			password : scope.password
+		};
+
+		console.log("postData: ", postData);
+		$http.post('php/register.php', postData).then(function(response) {
+			console.log("GEE WILKERS BATMAN THEY SAID: ", response.data);
+		}, function(response) {
+
+		});
+	}
+
+
+}]);
+
+myApp.controller('ScriptureCtrl', ['$scope', function(scope) {
+  scope.book = "1-ne";
+  scope.chapter = "10";
+  scope.verse = "15";
+  scope.source = "https://www.lds.org/scriptures/bofm/1-ne/2.1?lang=eng";
+  scope.repsonse ="";
+
 }]);
