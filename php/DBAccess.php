67d4bd6d843978c5da93d8cd59190ed0e92f5f7e
Ryan Gillett <RGillett7889@gmail.com>
2015-09-21 20:50:06 -0500

    test

--- php/DBAccess.php
+++ php/DBAccess.php
@@ -10,11 +10,13 @@ class DBAccess
 	
 }
 
-echo class_exists(PDO);
+echo "We have PDO: " . class_exists(PDO) . "?";
+echo "We have mysql: " . function_exists(mysql_connect) . "?";
+echo "We have mysqli: " . class_exists(mysqli) . "?";
 
 
 
-if (!$link = mysql_connect('localhost', 'php', 'php-pass')) {
+/*if (!$link = mysql_connect('localhost', 'php', 'php-pass')) {
     echo 'Could not connect to mysql';
     exit;
 }
@@ -22,5 +24,5 @@ if (!$link = mysql_connect('localhost', 'php', 'php-pass')) {
 if (!mysql_select_db('social_scriptures', $link)) {
     echo 'Could not select database';
     exit;
-}
+}*/
 ?>
